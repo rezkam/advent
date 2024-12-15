a49094b2ede7c1f104ab246cdf62055731aae952
Rez <hi@rez.nu>
2024-12-15 17:29:02 +0100

    Add initial implementation for Day 3 puzzle, including input handling and multiplication logic.

---
+++ 2024/3/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Regex pattern to match `mul(X,Y)` where X and Y are 1-3 digit numbers
+	// submatch 1: X and submatch 2: Y
+	mulPattern := `mul\((\d{1,3}),(\d{1,3})\)`
+
+	// Regex to match do(), don't(), and mul(X,Y) instructions
+	// in case of mul(X,Y) submatch 1: X and submatch 2: Y
+	doPattern := `do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)`
+
+	// Compile the regex
+	multRe := regexp.MustCompile(mulPattern)
+	doRe := regexp.MustCompile(doPattern)
+
+	totalMulResult := 0
+	totalDoMulResult := 0
+	mulEnabled := true // state should persist across lines of input
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		memLine := scanner.Text()
+		matches := multRe.FindAllStringSubmatch(memLine, -1)
+
+		for _, match := range matches {
+			if len(match) == 3 {
+				// Convert the matched strings to integers
+				x, y := convertToIntegers(match[1], match[2])
+				totalMulResult += x * y
+			}
+		}
+
+		matches = doRe.FindAllStringSubmatch(memLine, -1)
+		for _, match := range matches {
+			fullMatch := match[0]
+			if fullMatch == "do()" {
+				mulEnabled = true
+			} else if fullMatch == "don't()" {
+				mulEnabled = false
+			} else if len(match) == 3 && mulEnabled {
+				// Convert the matched strings to integers
+				x, y := convertToIntegers(match[1], match[2])
+				totalDoMulResult += x * y
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Total multiplication result: %d", totalMulResult)
+	log.Printf("Total multiplication result with do(): %d", totalDoMulResult)
+}
+
+func convertToIntegers(x, y string) (int, int) {
+	xInt, err1 := strconv.Atoi(x)
+	yInt, err2 := strconv.Atoi(y)
+	if err1 != nil {
+		log.Fatalf("Failed to convert X: %s, error: %v", x, err1)
+	}
+	if err2 != nil {
+		log.Fatalf("Failed to convert Y: %s, error: %v", y, err2)
+	}
+	return xInt, yInt
+}
